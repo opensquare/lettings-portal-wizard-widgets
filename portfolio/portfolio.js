0a0bede7af96d72a311379203a91c8ca3ba24ffa
danlenharris <daniel.harris@opensquare.co.uk>
2013-06-27 17:20:01 +0100

    Add property user button

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -9,12 +9,21 @@ function Widget_portfolio() {
 			});
 
 			$(this).addClass('selected');
-
+			$(".tabs").removeAttr("addProperty");
 			$(".tabs").attr("propertyUid", property);
+
 			pw.notifyChannelOfEvent("property_info");
-			$(".tabs > section").each(function(){
-					$(".noSelection").remove();
+		});
+
+		$(".addProperty").click(function(){
+			$(".property").each(function(){
+				$(this).removeClass('selected');
 			});
-		})
+			
+			$(".tabs").removeAttr("propertyUid");
+			$(".tabs").attr("addProperty", "true");
+
+			pw.notifyChannelOfEvent("property_info");
+		});
 	}
 }
