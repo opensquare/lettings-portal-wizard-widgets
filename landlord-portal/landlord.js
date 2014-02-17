ef45aee27745171e3bafe5984cce7a1b34d5faf5
Lewis Gallacher <lewis.gallacher@opensquare.co.uk>
2014-02-17 12:20:48 +0000

    Added progress summary to property summary widget, and new load page channel handler to landlord-portal

--- landlord-portal/landlord.js
+++ landlord-portal/landlord.js
@@ -9,20 +9,26 @@ $(document).ajaxError(function(event, xhr, settings, thrownError) {
 });
 
 function Widget_landlord_portal(){
-	
-	var widget = this
-	
-	/* channels */
-	var channelSetPageTitles = 'landlord-portal.setPageTitles';
-	var channelSetPageArgs = 'landlord-portal.setPageArgs';
-	var channelClosePage = 'landlord-portal.closePage';
-	var channelLoadFailed = 'loadFailed';
 
-	nav = {
-		homeContent: '',
-		homeId: 'home',
-		mountCache: {}
-	}
+    var 
+        widget = this,
+        // channels
+        channelSetPageTitles = 'landlord-portal.setPageTitles',
+        channelSetPageArgs = 'landlord-portal.setPageArgs',
+        channelClosePage = 'landlord-portal.closePage',
+        channelLoadPage = 'landlord-portal.loadPage',
+        channelLoadFailed = 'loadFailed',
+        // navigation state 
+        nav = {
+            homeContent: '',
+            homeId     : 'home',
+            type    : '',
+            subType : '',
+            pageId  : '',
+            pageArgs: '',
+            mountCache : {}
+        }
+    ;
 
 	this.widgetPrefix = 'll-';
 	this.errorMessage = "The page requested is not available, there may have been a problem loading or the content doesn't exist here yet";
@@ -36,6 +42,7 @@ function Widget_landlord_portal(){
 		pw.addListenerToChannel(this, channelSetPageArgs);
 		pw.addListenerToChannel(this, channelClosePage);
 		pw.addListenerToChannel(this, channelLoadFailed);
+		pw.addListenerToChannel(this, channelLoadPage);
 		/*hook up page load handler*/
 		$(window).on('hashchange', urlParse);
 		/*load page*/
@@ -50,6 +57,8 @@ function Widget_landlord_portal(){
 			updatePageArgs(event.pageId, event.args);
 		} else if (channel == channelClosePage){
 			window.location.hash = '';
+		} else if (channel == channelLoadPage) {
+            changePage(event);
 		} else if (channel == channelLoadFailed && nav.mountCache[event.name]) {
 			nav.mountCache[event.name] = false;
 			this.loadFailed(event.name);
@@ -63,51 +72,85 @@ function Widget_landlord_portal(){
 			// Remove first #
 			hashtag = hashtag.substr(1);
 
-			var hashtagParts = hashtag.split('?');
-			var pageId = hashtagParts[0];
-			var pageArgs = hashtagParts[1];
-
-			var idParts = pageId.split('/');
-			var type = idParts[0];
-			var subType = idParts[1];
-			if(pageId != '' && pageId !== nav.homeId){
+            var 
+                hashtagParts = hashtag.split('?'),
+                pageId = hashtagParts[0],
+                idParts = pageId.split('/'),
+                newPage = {
+                    type    : idParts[0],
+                    subType : idParts[1],
+                    pageId  : pageId,
+                    pageArgs: hashtagParts[1]
+                }
+            ;
+			if(newPage.pageId != '' && newPage.pageId !== nav.homeId){
 				try{
-				loadContent(pageId, type, subType, pageArgs);
+					loadContent(newPage);
 				} catch(error) {
 					if(error.name == 'Error') {
 						console.error('Unknown page operation');
 					} else {
 						console.error(error.message);
 					}
-			}
+				}
 			} else {
-				loadHome(pageArgs);
+				loadHome(newPage.pageArgs);
 			}
-			} else {
+		} else {
 			loadHome();
 		}
 	}
 
-	function loadContent(pageId, type, subType, pageArgs){
-		var widgetName = widget.widgetPrefix + type + '-' + subType;
-		var newContent = $(widget.contentTemplate).attr('pageid', pageId);
-		if(!pw.defined(pageArgs)){
-			pageArgs = '';
-		}
-		
-			newContent.children('.widget')
-				.attr({
-					'name': widgetName,
-				'pageid': pageId,
-				'page.args': pageArgs
-				})
-				.data('page-type', type)
+    function changePage(newPage) {
+        if(!pw.defined(newPage.type)){
+            newPage.type = nav.type;
+        }
+        if(!pw.defined(newPage.subType)){
+            newPage.subType = nav.subType;
+        }
+        if(!pw.defined(newPage.pageArgs)){
+            newPage.pageArgs = nav.pageArgs;
+        }
+        newPage.pageArgs = (newPage.pageArgs !== '') ? '?' + newPage.pageArgs : newPage.pageArgs;
 
-		loadPage(newContent);
+        window.location.hash = '#' + newPage.type + '/' + newPage.subType + newPage.pageArgs;
+    }
+
+    function loadContent(newPage){
+        var widgetName = widget.widgetPrefix + newPage.type + '-' + newPage.subType,
+            newContent = $(widget.contentTemplate).attr('pageid', newPage.pageId)
+        ;
+        if(!pw.defined(newPage.pageArgs)){
+            newPage.pageArgs = '';
+        }
+
+        newContent.children('.widget')
+            .attr({
+                name       : widgetName,
+                pageid     : newPage.pageId,
+                'page.args': newPage.pageArgs
+            })
+            .data('page-type', newPage.type);
+
+        // update nav state;
+        nav.pageId = newPage.pageId;
+        nav.type = newPage.type;
+        nav.subType = newPage.subType;
+        nav.pageArgs = newPage.pageArgs;
+
+        // load page
+        loadPage(newContent);
 
 	}
 
 	function loadHome(pageArgs){
+        // reset nav state
+        nav.pageId = '';
+        nav.type = '';
+        nav.subType = '';
+        nav.pageArgs = pageArgs;
+
+        // load home page
 		$homeContent = $(nav.homeContent);
 		$homeContent.children('.widget')
 					.attr('page.args', pageArgs)
@@ -149,13 +192,13 @@ function Widget_landlord_portal(){
 			// pull the title fron the widget div
 			var pulledTitle = article.children('div.widget').attr('page.title')
 			data.title = pulledTitle ? pulledTitle : '';
-			}
+		}
 
 		if (!data.subtitle) {
 			// default the subtitle
 			data.subtitle = '';
 		}
-		
+
 		var $title = article.find('.pagetitle')
 						.addClass('clearfix')
 						.html('<h2 class="pull-left">' + data.title + ' <small>' + data.subtitle + '</small></h2>');
--- property-summary/propertySummary.js
+++ property-summary/propertySummary.js
@@ -1,52 +1,101 @@
 function Widget_property_summary(){
 
-	var propertyId;
-	var widgetDiv;
+    var
+        docstoreThumbnailPath = 'docstore/property/{id}/thumbnail.jpg',
+        channelLoadPage = 'landlord-portal.loadPage',
+        channelPropertyInfo = 'property_info';
 
-	var docstoreThumbnailPath = 'docstore/property/{id}/thumbnail.jpg';
-	var channelPropertyInfo = 'property_info';
+    var
+        widgetObject,
+        articleTpl;
 
-	this.onReadyExtend = function() {
-		propertyId = this.$widgetDiv.data('propid');
-		widgetDiv = this.$widgetDiv;
-		this.loadProperty();
-	}
+    this.onReadyExtend = function() {
+        var propertyId = this.$widgetDiv.data('propid');
+        widgetObject   = this;
+        articleTpl     = $('article', this.$widgetDiv);
 
+        if (pw.defined(propertyId)) {
+            this.loadProperty(propertyId);
+        } else {
+            this.setContent('Property summary - no property set');
+        }
+    };
 
-	this.loadProperty = function() {
-		var widgetObject = this;
-		$.ajax({
-			url: 'widgets/portfolio/properties.json',
-			dataType: 'json',
-		})
-		.done(function(data) {
-			var properties = data.properties;
-			var articleTpl = $('article', widgetDiv);
-			for (var i = 0; i < properties.length; i++) {
-				var property = properties[i].property;
-				if (propertyId == property.id){
-					// populate article
-					property.image = docstoreThumbnailPath.replace('{id}', property.id);
-					$('.image div', articleTpl).text(property.image);
-					$('summary .address', articleTpl).html(property.address.addressLine1);
-					$('summary .sub', articleTpl).text(property.subtitle);
-					$('summary .description', articleTpl).text(property.summary);
-					$('summary .rentalStatus', articleTpl).text(property.status);
-					$('section>div', articleTpl).text(property.description);
-					widgetObject.addHandlers(articleTpl, property);
-				}
-				
-			};
-		})
-		.fail(function(object, status, error){
-			console.debug('error: ' + status + ' msg: ' + error);
-		});
-	}
+    this.createActionButton = function(propertyId, action){
+        var
+            $button = $('<button type="button" class="btn"></button>'),
+            complete = action.totalSteps == action.completed ? 100 : Math.ceil((action.completed / action.totalSteps) * 100),
+            stepText = action.action + ': ' + action.completed + '/' + action.totalSteps + ' complete';
 
-	this.addHandlers = function(propertySelector, property){
-		$('.image, summary .address', propertySelector).click(function(){
-			window.location.hash = 'property/details?' + propertyId;
-			pw.notifyChannelOfEvent(channelPropertyInfo, {id: property.id, address: property.address, status: property.status});
-		})
-	}
+        $button.text(stepText);
+        if (complete === 0) {
+            $button.addClass('disabled btn-default');
+        } else {
+            $button
+                .addClass('btn-primary')
+                .css('background-color', 'rgba(50,118,177,' + complete / 100 + ')') //color taken from bootstrap btn-primary
+                .click(function(event) {
+                    var newPage = {
+                        type: 'property',
+                        subType : action.portalWidget,
+                        pageArgs: propertyId
+                    }
+                    pw.notifyChannelOfEvent(channelLoadPage, newPage);
+                });
+        }
+        if (complete <= 50) {
+            $button.css('color', '#000');
+        }
+
+        return $button;
+    }
+
+    this.populateProperty = function(property){
+        var $actionContainer = $('.ps-actions', articleTpl);
+
+        // load thumbnail
+        property.image = docstoreThumbnailPath.replace('{id}', property.id);
+
+        // set main text
+        $('.image div', articleTpl).text(property.image);
+        $('summary .address', articleTpl).html(property.address.addressLine1);
+        $('summary .sub', articleTpl).text(property.subtitle);
+        $('summary .description', articleTpl).text(property.summary);
+        $('summary .rentalStatus', articleTpl).text(property.status);
+        $('section>div', articleTpl).text(property.description);
+
+        // populate action summary
+        var i;
+        for (i = 0; i < property.actions.length; i++) {
+            $actionContainer.append(this.createActionButton(property.id, property.actions[i]));
+        }
+        this.addHandlers(property);
+    }
+
+    this.loadProperty = function(propertyId) {
+        $.ajax({
+            url: 'widgets/portfolio/properties.json',
+            dataType: 'json',
+        })
+        .done(function(data) {
+            var properties = data.properties;
+            for (var i = 0; i < properties.length; i++) {
+                var property = properties[i].property;
+                if (propertyId == property.id){
+                    widgetObject.populateProperty(property);
+                }
+            }
+        })
+        .fail(function(object, status, error){
+            console.debug('error: ' + status + ' msg: ' + error);
+            widgetObject.setContent('Problem encountered whilst loading property');
+        });
+    }
+
+    this.addHandlers = function(property){
+        $('.image, summary .address', articleTpl).click(function(){
+            window.location.hash = 'property/details?' + property.id;
+            pw.notifyChannelOfEvent(channelPropertyInfo, {id: property.id, address: property.address, status: property.status});
+        });
+    }
 }
