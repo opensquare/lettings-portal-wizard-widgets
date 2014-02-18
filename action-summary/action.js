72253f78d8afc796951beb2c823edd63878d8655
Lewis Gallacher <lewis.gallacher@opensquare.co.uk>
2014-02-18 09:42:39 +0000

    added personal details action summary and new property action to main page

--- action-summary/action.js
+++ action-summary/action.js
@@ -1,18 +1,25 @@
 function Widget_action_summary(){
 
-	var template ,
-		pageType = ''
+    var widgetObject = this;
 
-	this.propertyId = '';
+    var template,
+        pageType = ''
+    ;
 
-	var widgetObject = this;
 
-	this.onReadyExtend = function(){
-		template = $('.action-container', this.$widgetDiv);
-		pageType = this.$widgetDiv.data('page-type');
-		this.propertyId = this.$widgetDiv.data('propid');
-		this.loadAction();
-	}
+    this.entityId = '';
+    this.mimimise;
+
+
+    this.onReadyExtend = function(){
+        template = $('.action-container', this.$widgetDiv);
+
+        pageType      = this.$widgetDiv.data('page-type');
+        this.entityId = this.$widgetDiv.data('propid');
+        this.minimise = this.$widgetDiv.data('minimise');
+
+        this.loadAction();
+    }
 
 	this.loadAction = function(){
 		$.ajax({
@@ -22,7 +29,7 @@ function Widget_action_summary(){
 		.done(function(data){
 			for (var i = 0; i < data.length; i++) {
 				activity = data[i];
-				if (activity.property == widgetObject.propertyId){
+				if (activity.entity == widgetObject.entityId){
 					populateActionList(activity);
 				}
 			};
@@ -32,45 +39,98 @@ function Widget_action_summary(){
 		});
 	}
 
+    function createCompletedStepItem(action, linkHref){
+        var link = 
+            '<li class="list-group-item list-group-item-success">'
+        +       '<span class="glyphicon glyphicon-ok-circle"></span>'
+        +       '&nbsp;<a class="text-success" href="' + linkHref + '">' + action.name + '</a>'
+        +   '</li>';
+        return link;
+    }
 
-	function populateActionList(data) {
-		var actionTemplate = '<li class="list-group-item"><span class="glyphicon"></span></li>';
-		$('.action-title', template).text(data.process.name)
-		$('.action-subtext', template).text(data.process.subtext)
-		var completed = 0;
-		var actions = data.process.actions;
-		var noOfActions = actions.length;
+    function createIncompleteStepItem(action, linkHref){
+        var link = 
+            '<li class="list-group-item list-group-item-default">'
+        +       '<span class="glyphicon glyphicon-record"></span>'
+        +       '&nbsp; <a class="text-primary" href="' + linkHref + '">' + action.name + '</a>'
+        +   '</li>';
+        return link;
+    }
 
-		for (var i = 0; i < noOfActions; i++) {
-			var action = actions[i];
-			var $actionTask = $(actionTemplate);
-			var actionLink = '';
-			var linkHref = '#' + pageType + '/' + action.portalWidget + '?' + action.params + '&p=' + widgetObject.propertyId;
+    function addMinifyFunctionality(template, completed) {
+        var
+            buttonHTML = '<button type="button" class="btn btn-link">'
+            +    '<span class="btn-text"></span>'
+            +    '</button>',
+            $button     = $(buttonHTML),
+            $stepText   = $('p.action-progress', template),
+            $subText    = $('.action-subtext', template),
+            $actionList = $('.action-group', template)
+        ;
 
-			if (action.complete){
-				completed++;
-				actionLink = '&nbsp;<a class="text-success" href="' + linkHref + '">' + action.name + '</a>'
-				$actionTask.addClass('list-group-item-success');
-			} else {
-				actionLink = '&nbsp;<a class="text-danger" href="' + linkHref + '">' + action.name + '</a>';
-				$actionTask.addClass('list-group-item-danger');
-			}
+        $('.btn-text', $button).html($stepText.html());
 
-			$('.glyphicon', $actionTask)
-			.addClass(action.complete ? 'glyphicon-ok':'glyphicon-remove')
-			.after(actionLink);
-			$('.action-list',template).append($actionTask);
-		}
+        $button.click(function(event) {
+            $actionList.slideToggle();
+        });
 
-		if (completed != noOfActions){
+        $subText.after($button);
+        $stepText.remove();
+        
+        $actionList.hide();
+
+    }
+
+    function populateActionList(data) {
+        var 
+            actionTemplate = '<li class="list-group-item"><span class="glyphicon"></span></li>',
+            completed = 0,
+            actions = data.process.actions,
+            noOfActions = actions.length
+        ;
+
+        // set main text
+        $('.action-title', template).text(data.process.name)
+        $('.action-subtext', template).text(data.process.subtext)
+
+        // build steps list
+        for (var i = 0; i < noOfActions; i++) {
+            var 
+                action      = actions[i],
+                $actionTask = $(actionTemplate),
+                actionLink  = '',
+                linkHref    = '#' + pageType + '/' + action.portalWidget + '?' + action.params + '&p=' + widgetObject.entityId;
+
+            if (action.complete){
+                completed++;
+                actionLink = createCompletedStepItem(action, linkHref);
+            } else {
+                actionLink = createIncompleteStepItem(action, linkHref);
+            }
+            $('.action-list',template).append(actionLink);
+        }
+
+		if (noOfActions !== 0){
 			updateProgress(template, completed, noOfActions);
-			$('.action-button', template).addClass('disabled').addClass('btn-default');
-		} else if (noOfActions > 0){
-			updateProgress(template, completed, noOfActions);
-			$('.action-button', template).addClass('btn-success')
 		} else {
 			$('.action-progress',template).remove();
+		}
+		if (widgetObject.minimise) {
+			$('.action-progress.progress', template).remove();
+			addMinifyFunctionality(template, noOfActions === completed);
+		}
+		updateActionButton(action, noOfActions, completed, data.process.toActionOn, template);
+	}
+
+	function updateActionButton(action, noOfActions, completed, actionOnComplete, template) {
+		if (!actionOnComplete || noOfActions == 0){
+			console.debug(noOfActions + ' ' + actionOnComplete);
 			$('.action-button', template).remove();
+		} else if (completed != noOfActions){
+			$('.action-button', template).addClass('disabled').addClass('btn-default');
+		} else if (noOfActions > 0){
+			$('.action-button', template).addClass('btn-success')
+			// todo add click handler
 		}
 	}
 
@@ -82,5 +142,5 @@ function Widget_action_summary(){
 		.css('width', Math.ceil(percentComplete) + '%')
 		.find('.action-percent').text(percentComplete.toString().substr(0,4));
 	}
-		
+
 }
