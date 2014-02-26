74b323045300cf771699010e7457cd8239bfda94
Paul Brownsmith <paulbrownsmith@pauls-mbp-2.home>
2014-02-26 15:55:35 +0000

    Portal page responsive work, now CSS driven and no longer requires javascript

--- portal-home/portal-home.js
+++ portal-home/portal-home.js
@@ -2,43 +2,6 @@ function Widget_portal_home(){
 
 	this.onReadyExtend = function() {
 
-		// handles the responsive classes depending on window size. Will need to re-visit as it doesn't behave exactly as it should.
-		handleWindowResize();
-		pw.addListenerToChannel(this, 'resize-panels');
-	}
-
-	this.handleEvent = function(channel,event) {
-		handleWindowResize();
-	}
-
-	handleWindowResize = function() {
-
-		var windowWidth = window.innerWidth;
-
-		if (windowWidth < 768) {
-			$('.panel-collapse').removeClass('in');
-		}
-
-		if (windowWidth > 768) {
-			$('.panel-collapse').addClass('in');
-		}
-
-		window.onresize = resize;
-
-		function resize() {
-
-			var windowWidth = window.innerWidth;
-
-			if (windowWidth < 768) {
-				$('.panel-collapse').removeClass('in');
-			}
-
-			if (windowWidth > 768) {
-				$('.panel-collapse').addClass('in');
-			}
-
-		}
-
 	}
 
 }
