49ab799b8f1b4c452d5790da7c54e2a056d1cb9e
Paul Brownsmith <paulbrownsmith@pauls-mbp-2.home>
2014-02-27 15:25:31 +0000

    User profile static html, link from portal homepage added to user profile page

---
+++ page-user-profile/user-profile-flow.js
@@ -0,0 +1,23 @@
+{
+  docBase: "/root",
+  formLists: {
+    main: [
+      {
+        id: "edit",
+        url: "user-profile-edit.html",
+        actions: [ "next" ]
+      },
+      {
+        id: "edit2",
+        url: "user-profile-edit2.html",
+        actions: [ "back" ],
+        actions: [ "next" ]
+      },
+      {
+        id: "view",
+        url: "user-profile-display.html",
+        actions: [ "back"]
+      }
+    ]
+  }
+}
