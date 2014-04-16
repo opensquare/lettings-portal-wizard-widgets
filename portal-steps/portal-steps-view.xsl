2b3da67ccf97cc2d1463825f47d183b3a65dd5ec
Lewis Gallacher <lewis.gallacher@opensquare.co.uk>
2014-04-16 09:41:52 +0100

    Simplified portal steps js

--- portal-steps/portal-steps-view.xsl
+++ portal-steps/portal-steps-view.xsl
@@ -2,18 +2,25 @@
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="2.0">
     <xsl:output method="html"/>
     <xsl:variable name="step" select="concat('{', 'step', '}')"/>
+
+    <!-- determine what should be displayed -->
     <xsl:variable name="activeStepNo">
         <xsl:choose>
+            <!--a specific step -->
             <xsl:when test="$step != '{step}' and not(empty(//entity[step='{step}']))">
-                <!-- specific step -->
                 <xsl:value-of select="'{step}'"/>
             </xsl:when>
+            <!--the first incomplete step -->
             <xsl:when test="count(//entity[status='incomplete']) &gt; 0">
-                <!-- first incomplete step -->
                 <xsl:value-of select="//entity[status='incomplete'][1]/step"/>
             </xsl:when>
-            <xsl:otherwise>
+            <!-- the first optional step -->
+            <xsl:when test="count(//entity[status='optional']) &gt; 0">
                 <xsl:value-of select="//entity[status='optional'][1]/step"/>
+            </xsl:when>
+            <!-- last complete step -->
+            <xsl:otherwise>
+                <xsl:value-of select="//entity[status='complete'][count(//entity[status='complete'])]/step"/>
             </xsl:otherwise>
         </xsl:choose>
     </xsl:variable>
@@ -43,7 +50,7 @@
                     <xsl:value-of select="'{entity}'"/>
                     <xsl:text>&amp;stepParams=</xsl:text>
                     <xsl:if test="//entity[step=$activeStepNo]/widgetParams">
-                        <!-- need to encode parameter string so it acts as a single parameter -->
+                        <!-- encode parameter string so it acts as a single parameter -->
                         <xsl:value-of select="replace(//entity[step=$activeStepNo]/widgetParams, '&amp;', '%26')"/>
                     </xsl:if>
                 </xsl:attribute>
@@ -68,12 +75,14 @@
                     <xsl:text>?</xsl:text>
                     <xsl:value-of select="replace(widgetParams, '&amp;', '%26')"/>
                 </xsl:attribute>
+
                 <xsl:attribute name="data-step"><xsl:value-of select="step"/></xsl:attribute>
+
                 <xsl:if test="disabled='true'">
                     <xsl:attribute name="data-disabled">true</xsl:attribute>
                 </xsl:if>
-                <xsl:variable name="status" select="status" />
 
+                <xsl:variable name="status" select="status" />
                 <xsl:choose>
                     <xsl:when test="$status = 'complete'">
                         <span class="glyphicon glyphicon-ok"></span>&#160;
@@ -104,7 +113,7 @@
                     <xsl:value-of select="completed"/> of <xsl:value-of select="total"/> steps complete)
                 </a>
             </li>
-            <li class="active">{pageDescription}</li>
+            <li class="active"><xsl:value-of select="//entity[step = $activeStepNo]/title"/></li>
         </ol>
     </xsl:template>
 
--- portal-steps/portal-steps.js
+++ portal-steps/portal-steps.js
@@ -3,21 +3,55 @@ function Widget_portal_steps(){
     /**
     *   Widget can prompt users before unloading widget, by default this is turned off. To enable/disable at any point from the loaded widget, 
     *   send a channel notification in the following format, replacing loadedWidgetName with the name of the loaded widget:
-    *   pw.notifyChannelOfEvent('loadedWidgetName.unloadPrompt', {enabled: true}) // pass false to disable
+    *   pw.notifyChannelOfEvent('loadedWidgetName.promptOnUnload', {enabled: true}) // pass false to disable
     *   Remember to disable again when appropriate
     */
 
-    var
-        updatePromptChannel,
-        stepSwitcher,
-        unloadChannel,
-        _this = this
+    this.displayType    = ''; // determines what set of steps to load
+    this.activeStep     = null; // jQuery object of current active step
+    this.portalChannels = {}; // channels used to send notifications to container
+    this.promptEnabled = false; // whether to prompt user before changing step
+
+    var _this = this,
+        confirmText = 'You are about to navigate away from the current step. Any changes you have made may be lost.',
+        publish = {
+            changeStep: 'step.load'
+        },
+        listen = {
+            unload: '' // channel listening for result of user prompt
+        }
     ;
 
-    this.prepareParams = function(display, pageData) {
-        // forms name of xml file to display. Needs updating when connecting to back end
+    /*
+        Before loading html
+    */
+
+    this.initExtend = function() {
+        // get page data from widget
+        var pageData    = this.$widgetDiv.data('page-data');
+        this.displayType = this.$widgetDiv.data('display');
         
-        var contentMap = {
+        // prepare request parameters
+        this.setLoadParameters(pageData);
+
+        // prepare listeners
+        listen.unload = this.name + '.unloadResult'
+        pw.addListenerToChannel(this, listen.unload);
+
+        this.portalChannels = pageData.portalChannels;
+
+        if (!pw.defined(this.portalChannels.promptOnPageChange)){
+            publish.promptUpdated = this.portalChannels.promptOnPageChange;
+        } else {
+            publish.promptUpdated = 'portal.preventPageLoad';
+        }
+
+    }
+
+    this.setLoadParameters = function(pageData) {
+        // Needs updating when connecting to back end
+        
+        var contentMap = { // temporary mapping to xml file
             user:     'page-user-profile',
             property: 'page-property-progress',
             tenancy:  'page-tenancy-progress'
@@ -29,218 +63,145 @@ function Widget_portal_steps(){
             this.parameterMap.entity = pageData.identifier;
         }
 
-        this.parameterMap.path = contentMap[display] + id;
+        this.parameterMap.path = contentMap[this.displayType] + id; // name of xml file
 
         if(pw.defined(pageData.arguments.step)) {
-            this.parameterMap.step = pageData.arguments.step;
+            this.parameterMap.step = pageData.arguments.step; // set which step to display
         }
     }
 
-    this.initExtend = function() {
-        // get page data from widget
-        var 
-            displayType = this.$widgetDiv.data('display'),
-            pageData    = this.$widgetDiv.data('page-data');
-        
-        // prepare request parameters
-        this.prepareParams(displayType, pageData);
+    /*
+        When ready to display
+    */
 
-        // prepare listeners
-        unloadChannel = this.name + '.unloadResult'
-        pw.addListenerToChannel(this, unloadChannel);
-
-        // initialise step change controller
-        if (typeof pageData.portalChannels == 'object'){
-            stepSwitcher = new StepSwitcher();
-            stepSwitcher.init(unloadChannel, pageData.portalChannels);
-        }
-    }
-
-	this.onReadyExtend = function() {
-
-        // add click handler to steps
-        this.addHandlers();
+    this.onReadyExtend = function() {
+        this.activeStep = $('.steps li.active a', this.$widgetDiv);
 
         // format disabled steps
         $('a.step-link[data-disabled=true]', this.$widgetDiv).addClass('disabled');
+        
+        if (!pw.defined(this.parameterMap.step)){
+            this.updateUrl(); // update query parameters if step not set to reflect step displayed
+        }
 
-        // get step to be loaded and add prompt listener
-        var currentStep = $('li.active a').attr('href');
-        currentStep = currentStep.substring(0, currentStep.indexOf('?'));
-        updatePromptChannel = stepSwitcher.setFirstStep(currentStep, $('li.active a').data('step'));
+        this.updatePromptListener();
+        updateUnloadHandler(this.promptEnabled);
 
-        pw.addListenerToChannel(this, updatePromptChannel);
+        // add link handlers
+        this.addHandlers();
+    }
+    
+    this.updateUrl = function(){
+        var number = this.activeStep.data('step');
+        if (pw.defined(this.portalChannels.setPageArgs)){
+            pw.notifyChannelOfEvent(this.portalChannels.setPageArgs, {
+                args : {step : number},
+                updateExisting : true,
+                reloadPage : false
+            })
+        } else {
+            console.error('Unable to update step parameter, change argument channel has not been supplied');
+        }
+    }
 
-        // replace breadcrumb placeholder
-        this.updatebreadCrumb($('li.active a', this.$widgetDiv).text().trim());
+    this.updatePromptListener = function() {
+        var activeStepTarget = this.activeStep.attr('href'),
+            name = activeStepTarget.substring(0, activeStepTarget.indexOf('?'));
 
+        if (pw.defined(listen.changePrompt)){
+            pw.removeListenersFromChannel(listen.changePrompt);
+        }
 
+        listen.changePrompt = name + '.promptOnUnload';
+        pw.addListenerToChannel(this, listen.changePrompt);
     }
 
     this.addHandlers = function() {
         $('a.step-link', this.$widgetDiv).click(function(){
-            var changing;
             
             if ($(this).data('disabled')) {
                 return false;
-            } else {
-                // send request to change step, which prompts first if neceesary
-                changing = stepSwitcher.changeStep(
-                    $(this).data('step'),
-                    $(this).attr('href'),
-                    _this.parameterMap.entity,
-                    onBeforeStepChange
-                )
             }
-
-            if (changing){
+            if(_this.requestChangeStep($(this))){
                 $(this).parents('.steps').children().removeClass('active');
                 $(this).parent().addClass('active');
-                _this.updatebreadCrumb($(this).text().trim());
+                _this.setNewStep($(this));
             }
 
             return false;
         });
+
     }
 
-    this.updatebreadCrumb = function(description) {
-        $('.breadcrumb li.active', this.$widgetDiv).text(description);
-    }
-
-    function onBeforeStepChange(newPromptChannel) {
-        // swap prompt listener to new step name
-        pw.removeListenersFromChannel(updatePromptChannel);
-        updatePromptChannel = newPromptChannel;
-        pw.addListenerToChannel(_this, updatePromptChannel);
-    }
-
-    this.handleEvent = function(channel, event){
-        if (channel == updatePromptChannel && pw.defined(event.enabled)) {
+    /*
+        On interaction with widget
+    */
+    this.handleEvent = function(channel, event) {
+        if (channel == listen.changePrompt && pw.defined(event.enabled)) {
             // update to enable/disable prompt
-            stepSwitcher.setPromptOnChange(event.enabled);
-        } else if(channel == unloadChannel && event) {
+            this.updatePromptHandler(event.enabled);
+        } else if(channel == listen.unload && event === true) {
             // portal unloading widget. Clean up listeners and handlers
-            stepSwitcher.beforeUnload(true);
+            this.widgetUnloading();
         }
     }
 
-    // keep step handling functionality separate
-    function StepSwitcher() {
-        var 
-            channels = {
-                load : 'step.load',
-                preventHashChange : 'portal.preventPageLoad',
-                stepUnload : '',
-                hashResponseChannel : '',
-                portalChannels : {}
-            },
-            promptOnChange = false,
-            confirmText = 'You are about to navigate away from the current step. Any changes you have made may be lost.'
-        ;
+    this.requestChangeStep = function($link) {
+        if (!this.promptEnabled || window.confirm(confirmText)) {
+            var stepToLoad = $link.attr('href').substring(0, $link.attr('href').indexOf('?'));
 
-        function setOnUnloadHandler() {
-            if (promptOnChange) {
-                window.onbeforeunload = function(){
-                    return confirmText;
-                };
-            } else {
-                window.onbeforeunload = null;
-            }
-        }
+            this.updatePromptHandler(false);
 
-        function setStepPromptChannel(name){
-            channels.stepUnload = name + '.unloadPrompt';
-        }
-
-        function publishPreventHashChange(){
-            /*  Inform listeners on whether to prompt before unloading,
-                what message to use and where to publish the result.
-            */ 
-            pw.notifyChannelOfEvent(channels.preventHashChange, {
-                preventLoad: promptOnChange,
-                message : confirmText,
-                responseChannel : channels.hashResponseChannel
+            pw.notifyChannelOfEvent(publish.changeStep, {
+                uri : $link.attr('href'),
+                entity : this.parameterMap.entity
             });
+            return true;
         }
-
-        function updateStepPageArg(number){
-            if (pw.defined(channels.portalChannels.setPageArgs)){
-                pw.notifyChannelOfEvent(channels.portalChannels.setPageArgs, {
-                    args : {
-                        step : number,
-                    },
-                    updateExisting : true,
-                    reloadPage : false
-                })
-            } else {
-                console.error('Unable to update step parameter, change argument channel has not been supplied');
-            }
-        }
-
-        function setPromptOnChange(prompt) {
-            // update whether prompt is required
-            if(promptOnChange !== prompt){
-                promptOnChange = prompt === true ? true : false;
-                // prompt has changed. Inform rest of portal of change
-                publishPreventHashChange();
-                setOnUnloadHandler();
-            }
-        }
-
-        function setFirstStep(name, stepNo) {
-            // initialise step channel without requesting load
-            setStepPromptChannel(name);
-            setOnUnloadHandler();
-            updateStepPageArg(stepNo);
-            publishPreventHashChange();
-            return channels.stepUnload;
-        }
-
-        function changeStep(stepNo, uri, uid, beforeStepChange) {
-            // confirm when required to change step, and if so, send request to change step
-            var newStep;
-            if (!(promptOnChange && !window.confirm(confirmText))) {
-                newStep = uri.substring(0, uri.indexOf('?'));
-                
-                beforeUnload();
-                setStepPromptChannel(newStep);
-                beforeStepChange(channels.stepUnload);
-
-                pw.notifyChannelOfEvent(channels.load, {
-                    uri    : uri,
-                    entity : uid
-                });
-                updateStepPageArg(stepNo);
-
-                return true;
-            }
-            return false;
-        }
-
-        // step is about to change or portal is unloading widget
-        function beforeUnload(hashUnload) {
-            setPromptOnChange(false);
-            if (channels.stepUnload !== ''){
-                pw.removeListenersFromChannel(channels.stepUnload);
-            }
-            setOnUnloadHandler();
-            if(hashUnload){
-                pw.removeListenersFromChannel(channels.hashResponseChannel);
-            }
-        }
-
-        function init(hashResponseChannel, portalChannels){
-            channels.hashResponseChannel = hashResponseChannel;
-            channels.portalChannels = portalChannels;
-        }
-
-        // expose public functions
-        return {
-            init : init,
-            setFirstStep : setFirstStep,
-            setPromptOnChange : setPromptOnChange,
-            changeStep : changeStep,
-            beforeUnload : beforeUnload
-        };
+        return false;
     }
+
+    this.setNewStep = function($link){
+        this.activeStep = $link;
+        $('.breadcrumb li.active', this.$widgetDiv).text($link.text().trim());
+        this.updateUrl();
+        this.updatePromptListener();
+    }
+
+    this.widgetUnloading = function() {
+        this.updatePromptHandler(false);
+        pw.removeListenersFromChannel(listen.changePrompt);
+        pw.removeListenersFromChannel(listen.unload);
+    }
+
+
+    /*
+        Prompt handlers
+    */
+    this.publishPromptStatus = function() {
+        pw.notifyChannelOfEvent(publish.promptUpdated, {
+            preventLoad: this.promptEnabled,
+            message: confirmText,
+            responseChannel: listen.unload
+        });
+    }
+
+    this.updatePromptHandler = function(enabled) {
+        if (this.promptEnabled !== enabled) {
+            this.promptEnabled = enabled ? true : false;
+            this.publishPromptStatus();
+            updateUnloadHandler(this.promptEnabled);
+        }
+    }
+
+    function updateUnloadHandler(enable) {
+        if (enable) {
+            window.onbeforeunload = function(){
+                return confirmText;
+            };
+        } else {
+            window.onbeforeunload = null;
+        }
+    }
+
 }
